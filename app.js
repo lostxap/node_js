1fedd48365856d5cf0dcbf75f6e8e606103dca98
lostxap <lostxap@gmail.com>
2020-06-13 23:48:09 +0300

    modificare_1

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req,res,next) => {
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next) => {
     console.log('in tha middleware');
-    next(); //alows the req to continue to next middleware 
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+
 });
 
-app.use((req,res,next) => {
-    console.log('in tha middleware');
+app.use('/product' ,(req,res,next) => {
+    console.log(req.body);
+    res.redirect('/');
+
+});
+
+app.use('/',(req,res,next) => {
+    res.send('<h1>Helo from express</h1>');
 });
 
 const server = http.createServer(app);
