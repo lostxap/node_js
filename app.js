d13675421d7f2486c05d7abbfbde8861610a712a
lostxap <lostxap@gmail.com>
2020-06-14 11:17:26 +0300

    am adaugat routes

--- app.js
+++ app.js
@@ -1,29 +1,18 @@
-const http = require('http');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product',(req,res,next) => {
-    console.log('in tha middleware');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+app.use(adminRoutes);
+app.use(shopRoutes);
 
+app.use((req,res,next) => {
+ res.status(404).send('<h1>page not founddd :(((((</h1>');
 });
 
-app.use('/product' ,(req,res,next) => {
-    console.log(req.body);
-    res.redirect('/');
-
-});
-
-app.use('/',(req,res,next) => {
-    res.send('<h1>Helo from express</h1>');
-});
-
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
